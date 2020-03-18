dd114e1134c084b72fc13640526f2a73745f6ae1
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-03-18 22:58:48 +0700

    Handle submit new task

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const getNewTaskText = () => {
   return document.getElementById("new-task-text").value
 }
+
+const newTaskForm = document.getElementById("new-task-form")
+
+const submitNewTask = () => {
+  const newTaskText = getNewTaskText()
+  alert(newTaskText)
+}
+
+newTaskForm.addEventListener("submit", submitNewTask)
