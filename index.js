3f9191e9036ae15befd99721c3bac7ee2404717d
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-03-18 23:32:34 +0700

    Handle insert task to list and prevent empty text

--- index.js
+++ index.js
@@ -2,11 +2,26 @@ const getNewTaskText = () => {
   return document.getElementById("new-task-text").value
 }
 
-const newTaskForm = document.getElementById("new-task-form")
-
-const submitNewTask = () => {
-  const newTaskText = getNewTaskText()
-  alert(newTaskText)
+const setNewTaskText = text => {
+  document.getElementById("new-task-text").value = text
 }
+
+const insertToTaskList = newTask => {
+  const taskList = document.getElementById("task-list")
+
+  taskList.innerHTML += `<li>${newTask}</li>`
+}
+
+// by default there is page refresh
+const submitNewTask = event => {
+  event.preventDefault()
+  const newTaskText = getNewTaskText()
+  if (newTaskText !== "") {
+    insertToTaskList(newTaskText)
+    setNewTaskText("")
+  }
+}
+
+const newTaskForm = document.getElementById("new-task-form")
 
 newTaskForm.addEventListener("submit", submitNewTask)
