7194be62bf645e411e046c9838f08f7725b95341
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-03-21 15:44:40 +0700

    Handle submit new task as object

--- index.js
+++ index.js
@@ -8,10 +8,30 @@ class task {
   }
 }
 
-const tasks = []
+let tasks = []
 
-const getNewTaskText = () => {
-  return document.getElementById("new-task-text").value
+const getNewTask = () => {
+  const newTaskID = () => {
+    if (tasks.length === 0) {
+      return (id = 0)
+    }
+    const newID = tasks
+      .map(id => this.id)
+      .reduce((max, value) => Math.max(max, value))
+    return newID
+  }
+  const newTaskText = document.getElementById("new-task-text").value
+  const newTaskFavorite = document.getElementById("new-task-favorite").value
+  const newTaskDate = document.getElementById("new-task-date").value
+  const newTaskTags = document.getElementById("new-task-tags").value
+  const newTask = new task(
+    newTaskID,
+    newTaskText,
+    newTaskFavorite,
+    newTaskDate,
+    newTaskTags
+  )
+  return newTask
 }
 
 const setNewTaskText = text => {
@@ -27,9 +47,11 @@ const insertToTaskList = newTask => {
 // by default there is page refresh
 const submitNewTask = event => {
   event.preventDefault()
-  const newTaskText = getNewTaskText()
+  const newTask = getNewTask()
+  const newTaskText = newTask.text
   if (newTaskText !== "") {
     insertToTaskList(newTaskText)
+    tasks.push(newTask)
     setNewTaskText("")
   }
 }
@@ -38,4 +60,4 @@ const newTaskForm = document.getElementById("new-task-form")
 
 newTaskForm.addEventListener("submit", submitNewTask)
 
-document.getElementById("date-picker").valueAsDate = new Date()
+// document.getElementById("new-task-date").valueAsDate = new Date()
