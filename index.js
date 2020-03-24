ddf631e40996730e386f84379b3f0b7a5fc132fb
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-03-24 21:36:39 +0700

    Refactor delete button, fix naming

--- index.js
+++ index.js
@@ -8,19 +8,53 @@ let tasks = []
 
 const newTaskForm = document.getElementById("new-task-form")
 const taskList = document.getElementById("task-list").getElementsByTagName("li")
-const taskRemoveButtons = document
-  .getElementById("task-list")
-  .getElementsByTagName("button")
 const buttonRemoveAll = document.getElementById("button-remove-all")
 
 // -----------------------------------------------------------------------------
 // Functions
 
-const displayTasksToTaskList = newTask => {
+const addDeleteFunctionality = () => {
+  const taskRemoveButtons = document.getElementsByClassName("delete")
+
+  for (let index = 0; index < taskRemoveButtons.length; index++) {
+    taskRemoveButtons[index].addEventListener("click", function() {
+      const deletedTaskID = this.parentNode.dataset.id
+      const deletedTaskIndex = tasks.findIndex(task => task.id == deletedTaskID)
+      tasks.splice(deletedTaskIndex, 1)
+      this.parentNode.remove()
+      syncTasks()
+    })
+  }
+}
+
+const displayTasks = () => {
+  const taskList = document.getElementById("task-list")
+
+  tasks.forEach(task => {
+    const taskTextElement = `<span>${task.text}</span>`
+    const deleteButtonElement = `<button class="delete">x</button>`
+    const favoriteToggleElement = `<input type="checkbox" ${
+      task.favorite ? "Checked" : ""
+    }/>`
+    const dueDateElement = `<time datetime="${task.date}">${task.date}</time>`
+
+    taskList.innerHTML += `
+      <li data-id="${task.id}">
+        ${taskTextElement}
+        ${dueDateElement}
+        ${favoriteToggleElement}
+        ${deleteButtonElement}
+      </li>`
+  })
+
+  addDeleteFunctionality()
+}
+
+const addTask = newTask => {
   const taskList = document.getElementById("task-list")
 
   const taskTextElement = `<span>${newTask.text}</span>`
-  const closeButtonElement = "<button>x</button>"
+  const deleteButtonElement = `<button class="delete">x</button>`
   const favoriteToggleElement = `<input type="checkbox" ${
     newTask.favorite ? "Checked" : ""
   }/>`
@@ -31,18 +65,10 @@ const displayTasksToTaskList = newTask => {
     ${taskTextElement}
     ${dueDateElement}
     ${favoriteToggleElement}
-    ${closeButtonElement}
+    ${deleteButtonElement}
   </li>`
 
-  for (index = 0; index < taskRemoveButtons.length; index++) {
-    taskRemoveButtons[index].addEventListener("click", function() {
-      const deletedTaskID = this.parentNode.dataset.id
-      const deletedTaskIndex = tasks.findIndex(task => task.id == deletedTaskID)
-      tasks.splice(deletedTaskIndex, 1)
-      this.parentNode.remove()
-      updateTasks()
-    })
-  }
+  addDeleteFunctionality()
 }
 
 // -----------------------------------------------------------------------------
@@ -99,29 +125,30 @@ const setNewTaskText = text => {
   document.getElementById("new-task-date").value = text
 }
 
-// by default there is page refresh
 const submitNewTask = event => {
+  // by default there is page refresh
   event.preventDefault()
   const newTask = getNewTask()
   const newTaskText = newTask.text
 
   if (newTaskText !== "") {
-    displayTasksToTaskList(newTask)
+    addTask(newTask)
     tasks.push(newTask)
     setNewTaskText("")
-    updateTasks()
+    syncTasks()
   }
 }
 
 // function to run after editing array "tasks"
-const updateTasks = () => {
+const syncTasks = () => {
   localStorage.setItem("myTasks", JSON.stringify(tasks))
 }
 
-buttonRemoveAll.onclick = () => {
+const removeAllTasks = () => {
   const isRemoveAll = confirm(
     "Are you sure to remove all task? You cannot undo this"
   )
+
   if (isRemoveAll) {
     tasks = []
     localStorage.clear()
@@ -129,35 +156,16 @@ buttonRemoveAll.onclick = () => {
   }
 }
 
-// filter by tags
-const filterByTags = (...tags) => {
-  const newFiltered = []
-  const meshTags = [...tags]
-
-  for (let iteraTags = 0; iteraTags < meshTags.length; iteraTags++) {
-    for (let index = 0; index < tasks.length; index++) {
-      const bucket = tasks[index].tags.filter(
-        eachTag => eachTag.toLowerCase() === meshTags[iteraTags].toLowerCase()
-      )
-      if (bucket.length !== 0) {
-        newFiltered.push(tasks[index])
-      }
-    }
-  }
-
-  const uniqueFiltered = new Set(newFiltered)
-  return Array.from(uniqueFiltered)
-}
-
 // -----------------------------------------------------------------------------
 // Event Listeners
 
 newTaskForm.addEventListener("submit", submitNewTask)
+buttonRemoveAll.addEventListener("click", removeAllTasks)
 
 // -----------------------------------------------------------------------------
 // Initialization
 
 if (localStorage.getItem("myTasks") !== null) {
   tasks = JSON.parse(localStorage.getItem("myTasks"))
-  tasks.forEach(item => displayTasksToTaskList(item))
+  tasks.forEach(item => addTask(item))
 }
